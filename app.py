42c4d13710215f5c01013834d32045da70f6dc51
Marco Sampietro <sampietro.marco@iisgalvanimi.edu.it>
2024-05-23 16:16:29 +0000

    Correzione verifica

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import pandas as pd
+app = Flask(__name__)
+
+dati_clienti = pd.read_csv('/workspace/VerificaFlaskbase/data/dati_clienti.csv')
+
+@app.route('/')
+def homepage():
+    risultato = sorted(list(set(dati_clienti['Country'])))
+    return render_template('index.html', lista = risultato)
+
+@app.route('/elencocitta/<nazione>', methods = ['GET'])
+def citta(nazione):
+    info = dati_clienti[dati_clienti['Country'] == nazione]['City']
+    info = info.drop_duplicates()
+    return render_template('radiobutton.html', tabella = info)
+
+@app.route('/elencoclienti', methods = ['GET'])
+def clienti():
+    citta = request.args.get('citta')
+    info = dati_clienti[dati_clienti['City'] == citta]
+    return render_template('clienti.html', tabella = info.to_html())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=32457, debug=True)
